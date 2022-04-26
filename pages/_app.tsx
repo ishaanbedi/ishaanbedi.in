c5bc1a2c6187adddaa65c8907c6ba25a3fbcdf75
Ishaan Bedi <bediishaan6@gmail.com>
2022-04-26 08:19:09 +0530

    Installed nProgress for Nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 import '../styles/main.css';
+import NextNProgress from "nextjs-progressbar";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextNProgress />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp
