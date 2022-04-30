fcf80f730358dd93bdcc8222b6ca212706b46b96
Ishaan Bedi <bediishaan6@gmail.com>
2022-04-30 20:56:34 +0530

    Updated Font

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ import Stack from '../components/Stack';
 import Contact from '../components/Contact';
 import Footer from '../components/Footer';
 import styles from '../styles/Home.module.css';
+
+
 export default function Home() {
   return (
     <>
