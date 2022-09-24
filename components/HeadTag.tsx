72182d7e7e94b508c6fddf1d05afe9ddd7d36ff7
Ishaan Bedi <bediishaan6@gmail.com>
2022-09-24 17:56:45 +0530

    Fixed title fetching error

--- components/HeadTag.tsx
+++ components/HeadTag.tsx
@@ -3,7 +3,7 @@ const HeadTag = (props) => {
   return (
     <div>
       <Head>
-        <title>{props.name} | Developer</title>
+        <title>Ishaan Bedi | Developer</title>
         <meta
           name="viewport"
           content="initial-scale=1.0, width=device-width user-scalable=no "
